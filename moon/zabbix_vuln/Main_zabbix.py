2a238d00c9fed6fe47654ae5016c9ad127037b6a
rpkr <18302425732@163.com>
2018-11-19 20:00:02 +0800

    新增：zabbix_sql

---
+++ moon/zabbix_vuln/Main_zabbix.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+import zabbix_vuln.zabbix_sql_CVE_2016_10134
+
+
+
+def exec(URL):
+    zabbix_vuln.zabbix_sql_CVE_2016_10134.attack(URL)
+
+
+
+
+if __name__ == "__main__":
+    exec()
